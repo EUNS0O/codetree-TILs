dcd3f406327be11ff6501f8700ae81c76ca89873
EUNS0O <jangeunsu9505@gmail.com>
2024-06-20 19:05:34 +0900

    Title: 삽입 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240620/삽입 정렬 구현/implement-insertion-sort.cpp
+++ 240620/삽입 정렬 구현/implement-insertion-sort.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<vector>
+
 
 using namespace std;
 
@@ -8,23 +8,24 @@ int main() {
     cin>>n;
 
     int* arr = new int[n];
-    int x;
-
+    
     
     for(int i=0;i<n;i++){
-            cin>>x;
-            arr[i]=x;
+        cin>>arr[i];
     }
     
-    for(int i=0;i<n;i++){
-        int j = i+1;
-        while(j>0&&(arr[j-1]>arr[j])){
-            swap(arr[j-1],arr[j]);
+    for(int i=1;i<n;i++){
+        int j= i-1;
+        while(j>=0&&arr[j]>arr[j+1]){
+            swap(arr[j],arr[j+1]);
             j--;
         }
     }
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
+
+    delete[] arr;
     return 0;
 }
